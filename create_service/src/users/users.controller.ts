4785e23bd9a8a68dc560f00a9feea6535994e09d
wiblok <asakura.teiroku@gmail.com>
2023-07-11 19:48:57 +0900

    a

---
+++ create_service/src/users/users.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+
+interface User {
+  id: number;
+  name: string;
+}
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getUsers(): User[] {
+    return this.usersService.getUsers();
+  }
+
+  @Post()
+  addUser(@Body() name: string): void {
+    this.usersService.addUser(name);
+  }
+
+  @Put(':id')
+  putUser(@Param('id') id: number, @Body('name') name: string): void {
+    this.usersService.putUser(id, name);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: number): void {
+    this.usersService.deleteUser(id);
+  }
+}
